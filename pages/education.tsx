2ab5907bb5036885c4abd8b812ef72e347dbfded
Ahmed Abouelnaga <ahmedabouelnaga2004@gmail.com>
2025-01-02 01:30:32 -0500

    fixed a memory error

--- pages/education.tsx
+++ pages/education.tsx
@@ -74,7 +74,7 @@ export function EducationSection() {
               xmlns="http://www.w3.org/2000/svg"
               className="h-12 w-12 text-purple-700"
               fill="none"
-              viewBox="0 0 24 24"  // Fixed viewBox value
+              viewBox="0 0 24 24"
               stroke="currentColor"
               strokeWidth={2}
             >
@@ -101,7 +101,7 @@ export function EducationSection() {
               xmlns="http://www.w3.org/2000/svg"
               className="h-12 w-12 text-purple-700"
               fill="none"
-              viewBox="0 0 24 24"  // Fixed viewBox value
+              viewBox="0 0 24 24"
               stroke="currentColor"
               strokeWidth={2}
             >
